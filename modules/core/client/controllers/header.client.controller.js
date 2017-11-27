c18ce18b80d38c97e599972a6a19d2a456943d17
Erik Higginbotham <higgi615@ufl.edu>
2017-11-26 20:20:50 -0500

    fixed more stuff from the to-do list

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -15,20 +15,20 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
       $scope.isCollapsed = !$scope.isCollapsed;
     };
 
-// FIX THIS FUNCTION!!
-    $scope.logoRedirect = function (signedIn) {
-      if (!signedIn){
-        $state.go('home');
+// FIX THIS FUNCTION!! -- Fixed ;)
+    $scope.logoRedirect = function () {
+
+      if ($scope.authentication.user.roles.indexOf('Organization') >= 0) {
+        $state.go('orgDash.eventList');
+        console.log($state.current);
       }
+      else if ($scope.authentication.user.roles.indexOf('Business') >= 0) {
+        $state.go('bizDash.eventList');
+        console.log($state.current);
+      }
+
       else {
-        if ($scope.authentication.user.roles.indexOf('Organization') >= 0) {
-          $state.go('orgDash.eventList');
-          console.log($state.current);
-        }
-        else if ($scope.authentication.user.roles.indexOf('Business') >= 0) {
-          $state.go('bizDash.eventList');
-          console.log($state.current);
-        }
+        $state.go('home');
       }
     };
 
